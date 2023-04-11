6fc10f145f0d03781d5176cb39d4e885ef95148e
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-04-11 12:52:24 +0330

    Disable oveflow for the page

--- js/menu-popup.js
+++ js/menu-popup.js
@@ -2,5 +2,9 @@ const menuButton = document.querySelector('#menu-icon');
 const exitMenuButton = document.querySelector('#exit-menu-icon');
 const menuBtns = [menuButton, exitMenuButton];
 const menuContainer = document.querySelector('.mobile-menu-container');
+const whole_page = document.querySelector('html');
 
-menuBtns.map((menuBtn) => menuBtn.addEventListener('click', () => menuContainer.classList.toggle('mobile-menu-toggler')));
+menuBtns.map((menuBtn) => menuBtn.addEventListener('click', () => {
+  menuContainer.classList.toggle('mobile-menu-toggler');
+  whole_page.classList.toggle('overflow-none');
+}));
