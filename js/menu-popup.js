6453d1b8142446f660f19debaacaddfe505eccd7
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-04-11 13:10:29 +0330

    Fix eslint issues

--- js/menu-popup.js
+++ js/menu-popup.js
@@ -1,17 +1,17 @@
 const menuButton = document.querySelector('#menu-icon');
 const exitMenuButton = document.querySelector('#exit-menu-icon');
-const menuLinks = document.querySelectorAll('.mobile-menu-link')
+const menuLinks = document.querySelectorAll('.mobile-menu-link');
 const menuBtns = [menuButton, exitMenuButton];
 const menuContainer = document.querySelector('.mobile-menu-container');
-const whole_page = document.querySelector('html');
+const wholePage = document.querySelector('html');
 
 menuBtns.map((menuBtn) => menuBtn.addEventListener('click', () => {
   menuContainer.classList.toggle('mobile-menu-toggler');
-  whole_page.classList.toggle('overflow-none');
+  wholePage.classList.toggle('overflow-none');
 }));
 
-menuLinks.forEach((menuLink)=>{ menuLink.addEventListener("click",(e)=>{
-  console.log(e.target)
-  menuContainer.classList.toggle('mobile-menu-toggler');
-  
-})})
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener('click', () => {
+    menuContainer.classList.toggle('mobile-menu-toggler');
+  });
+});
