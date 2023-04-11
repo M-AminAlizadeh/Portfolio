f3c503e676d71df142cdc6080fe546d842f62a30
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-04-11 13:07:36 +0330

    Add class to all mobile menu links and add functionality to them for toggle the menu when user click on them

--- js/menu-popup.js
+++ js/menu-popup.js
@@ -1,5 +1,6 @@
 const menuButton = document.querySelector('#menu-icon');
 const exitMenuButton = document.querySelector('#exit-menu-icon');
+const menuLinks = document.querySelectorAll('.mobile-menu-link')
 const menuBtns = [menuButton, exitMenuButton];
 const menuContainer = document.querySelector('.mobile-menu-container');
 const whole_page = document.querySelector('html');
@@ -8,3 +9,9 @@ menuBtns.map((menuBtn) => menuBtn.addEventListener('click', () => {
   menuContainer.classList.toggle('mobile-menu-toggler');
   whole_page.classList.toggle('overflow-none');
 }));
+
+menuLinks.forEach((menuLink)=>{ menuLink.addEventListener("click",(e)=>{
+  console.log(e.target)
+  menuContainer.classList.toggle('mobile-menu-toggler');
+  
+})})
