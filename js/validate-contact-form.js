5499628ae0b7dc5e1de727398b0d89ec20d56f16
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-04-13 20:12:16 +0330

    Add functionality to check form email and show error message

---
+++ js/validate-contact-form.js
@@ -0,0 +1,11 @@
+const emailInput = document.querySelector("#email-input");
+const submitButton = document.querySelector("#submit-btn");
+const errorMessageContainer = document.querySelector(".error-message-container")
+
+submitButton.addEventListener("click",(e)=>{
+  if(emailInput.value.toLowerCase() !== emailInput.value){
+    // Show error
+    e.preventDefault();
+    errorMessageContainer.style.display = "block";
+    }
+})
