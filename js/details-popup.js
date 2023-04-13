f0d9516843a1e8208126d4ddd450381109bb1b5e
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-04-13 15:23:47 +0330

    Set condition in details popup for diffrent screen sizes

--- js/details-popup.js
+++ js/details-popup.js
@@ -3,15 +3,20 @@ const exitBtn = document.querySelector('.mobile-details-first-row-container img'
 const mobileDetailsPopupBgContainer = document.querySelector('.mobile-details-popup-bg-container');
 
 // Mobile
-// exit button functionality
-exitBtn.addEventListener('click', () => {
-  mobileDetailsPopupBgContainer.style.display = 'none';
-});
-
-// each project show info functionality
-seeProjectBtns.forEach((seeProjectBtn) => {
-  seeProjectBtn.addEventListener('click', () => {
-    // show popup
-    mobileDetailsPopupBgContainer.style.display = 'block';
+if(window.innerWidth <= 375){
+  // exit button functionality
+  exitBtn.addEventListener('click', () => {
+    mobileDetailsPopupBgContainer.style.display = 'none';
   });
-});
+  
+  // each project show info functionality
+  seeProjectBtns.forEach((seeProjectBtn) => {
+    seeProjectBtn.addEventListener('click', () => {
+      // show popup
+      mobileDetailsPopupBgContainer.style.display = 'block';
+    });
+  });
+}else{
+// Desktop
+  
+}
