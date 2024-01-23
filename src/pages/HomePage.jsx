18cfbf370750c5e22cb7cec2b871b8e5e06277ee
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2024-01-23 17:19:06 +0330

    Import all components

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,22 @@
-const HomePage = () => <p>Home page</p>
+import Navbar from '../components/Navbar'
+import Intro from '../components/Intro'
+import Projects from '../components/Projects'
+import AboutMe from '../components/AboutMe'
+import ContactMe from '../components/ContactMe'
+import Footer from '../components/Footer'
+
+
+const HomePage = () => {
+  return(
+    <>
+      <Navbar />
+      <Intro />
+      <Projects />
+      <AboutMe />
+      <ContactMe />
+      <Footer />
+    </>
+  )
+}
 
 export default HomePage;
