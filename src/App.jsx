7f5ee088a863bae80d8fbb8dbf3f033e588f9ea1
m-aminalizadeh <aminalizadeh@riseup.net>
2024-01-23 14:22:47 +0330

    Add router list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-function App() {
-  return (
-    <>
-      portfolio
-    </>
-  )
-}
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+import HomePage from './pages/HomePage';
+import BlogPage from './pages/BlogPage';
+
+const App = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/blog',
+    element: <BlogPage />,
+  },
+])
 
 export default App
