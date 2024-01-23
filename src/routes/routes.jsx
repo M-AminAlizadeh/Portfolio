0024371ea9580c62796a13aa3fb4bd3c52a91eed
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2024-01-23 16:08:32 +0330

    Add routes file for routing between pages

---
+++ src/routes/routes.jsx
@@ -0,0 +1,18 @@
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+import HomePage from '../pages/HomePage';
+import BlogPage from '../pages/BlogPage';
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/blog',
+    element: <BlogPage />,
+  },
+])
+
+export default routes;
