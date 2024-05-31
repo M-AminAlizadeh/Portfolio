bfe3d199d8ba59bbaec418b269815282587c65c6
m-aminalizadeh <aminalizadeh@riseup.net>
2024-05-31 14:41:53 +0330

    Add responsive navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+
+const Header = () => {
+  return(
+    <Navbar fluid rounded>
+      <NavbarBrand href="/">
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Amin Alizadeh</span>
+      </NavbarBrand>
+      <NavbarToggle />
+      <NavbarCollapse>
+        <NavbarLink href="/blog">Blogs</NavbarLink>
+        <NavbarLink href="#projects">Projects</NavbarLink>
+        <NavbarLink href="#about-me">About me</NavbarLink>
+        <NavbarLink href="#contact-me">Contact me</NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
+  )
+}
+
+export default Header;
