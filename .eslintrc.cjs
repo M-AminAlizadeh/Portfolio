1ca2b2c88446ddf68e12a93da4dd7d873960c7d2
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-03 11:49:31 +0330

    Add eslint-prettier packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
