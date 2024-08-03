41b780585c1aac49c782e6ddb47f2c0fe6a8706b
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-03 13:55:56 +0330

    chore: Update navbar component to display author name instead of logo text

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ function Navbar() {
               alt="Flowbite Logo"
             />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+              Amin Alizadeh
             </span>
           </Link>
           <button
@@ -49,10 +49,18 @@ function Navbar() {
             <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
               <li>
                 <Link
+                  to="/blog"
+                  className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Blogs
+                </Link>
+              </li>
+              <li>
+                <Link
                   to="/projects"
                   className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
-                  Services
+                  Projects
                 </Link>
               </li>
               <li>
@@ -76,7 +84,7 @@ function Navbar() {
         </div>
       </nav>
     </div>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
