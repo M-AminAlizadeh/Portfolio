2d43fd66acebcfe1bf1871572c4fb7d60278e50c
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-06 17:06:48 +0330

    Add style to the content of Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,25 @@ import { TypeAnimation } from 'react-type-animation'
 
 function Home() {
   return (
-    <div className="flex justify-between">
-      <article>
-        <h1>Hello I&apos;m Amin</h1>
-        <p>
+    <div className="h-screen overflow-hidden relative">
+      <div className="absolute top-0 left-0 w-full h-full">
+        <ThreeScene />
+      </div>
+      <article className="relative p-5">
+        <h1 className="text-3xl">
+          Hello👋🏼
+          <div>Welcome to my portfolio</div>
+          <br />
+          I&apos;m
+          <TypeAnimation
+            sequence={['Amin Alizadeh', 2000]}
+            wrapper="span"
+            cursor={true}
+            repeat={Infinity}
+            className="m-1 bg-red-500 px-1 text-white"
+          />
+        </h1>
+        <p className="text-3xl">
           I&apos;m a
           <TypeAnimation
             sequence={[
@@ -21,11 +36,10 @@ function Home() {
             wrapper="span"
             cursor={true}
             repeat={Infinity}
-            className="text-blue-500"
+            className="text-blue-500 m-1 text-3xl font-bold"
           />
         </p>
       </article>
-      <ThreeScene />
     </div>
   )
 }
