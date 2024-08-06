2a25a92086027699c2dca3e3b3c1c686f0c259d3
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-06 16:08:00 +0330

    chore: Update Home page with animated introduction and 3D scene

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import ThreeScene from '../ThreeScene'
+import { TypeAnimation } from 'react-type-animation'
 
 function Home() {
-  return <div>Home</div>
+  return (
+    <div className="flex justify-between">
+      <article>
+        <h1>Hello I&apos;m Amin</h1>
+        <p>
+          I&apos;m a
+          <TypeAnimation
+            sequence={[
+              'Software Engineer',
+              1500,
+              'Frontend Developer',
+              1500,
+              'Fullstack Developer',
+              1500,
+              'Backend Developer',
+            ]}
+            wrapper="span"
+            cursor={true}
+            repeat={Infinity}
+            className="text-blue-500"
+          />
+        </p>
+      </article>
+      <ThreeScene />
+    </div>
+  )
 }
 
 export default Home
