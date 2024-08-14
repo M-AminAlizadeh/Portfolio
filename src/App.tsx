496cbd8058850ab5e499d9f30748bebb20c38d73
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-14 16:33:43 +0330

    feat: import and use the HomePage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import HomePage from './pages/HomePage';
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <HomePage />
     </>
   );
 }
