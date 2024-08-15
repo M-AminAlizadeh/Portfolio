8d5cbc7b1c2eac81312c8871cbe0feb8884032e8
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-15 10:04:28 +0330

    refactor: Update Layout component with improved styling

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,7 @@
-import Header from "./Header"
-import {
-  Link,
-} from "react-router-dom";
+import Header from './Header';
+import { Link } from 'react-router-dom';
 
-const Layout = ({element}: {element: React.ReactNode}) => {
+const Layout = ({ element }: { element: React.ReactNode }) => {
   return (
     <div className="border-2 min-h-[90vh] m-10 p-10 grid grid-cols-5 grid-rows-6">
       <Header />
@@ -23,12 +21,11 @@ const Layout = ({element}: {element: React.ReactNode}) => {
           </li>
         </ul>
       </nav>
-      <main className="border-2 border-blue-700 col-span-4 row-span-5">
+      <main className="border-2 border-blue-700 col-span-4 row-span-5 p-8">
         {element}
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
-
+export default Layout;
