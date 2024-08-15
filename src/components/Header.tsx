b51878722f18550f49234b5ceee0ed36238568fd
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-15 10:17:27 +0330

    refactor: Remove border from Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="font-mono border-2 col-span-5 row-span-1">
+    <header className="font-mono col-span-5 row-span-1">
       <h1 className="text-6xl">Amin Alizadeh</h1>
       <h4 className="italic capitalize">software engineer</h4>
     </header>
