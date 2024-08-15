9511e17278961eca87646c2ddd0d2275641ecc5b
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-15 09:06:07 +0330

    feat: Improve website layout with reusable Header component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,9 @@
+import Header from "./Header"
+
 const Layout = () => {
   return (
     <div className="border-2 min-h-[90vh] m-10 p-10 grid grid-cols-5 grid-rows-6">
-      <header className="font-mono border-2 col-span-5 row-span-1">
-        <h1 className="text-6xl">Amin Alizadeh</h1>
-        <h4 className="italic capitalize">software engineer</h4>
-      </header>   
+      <Header />
       <nav className="border-2 border-red-600 col-span-1 row-span-5">
         <ul className="flex flex-col gap-2">
           <li>
