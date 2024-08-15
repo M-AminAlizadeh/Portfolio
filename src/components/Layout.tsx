3015f536a4f279ae42e8f7602edcd37d759ca0fb
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-15 10:24:04 +0330

    refactor: Add Main component to Layout for improved code organization

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import Header from './Header';
+import Main from './Main';
 import Navbar from './Navbar';
 
 const Layout = ({ element }: { element: React.ReactNode }) => {
@@ -6,9 +7,7 @@ const Layout = ({ element }: { element: React.ReactNode }) => {
     <div className="border-2 min-h-[90vh] m-10 p-10 grid grid-cols-5 grid-rows-6">
       <Header />
       <Navbar />
-      <main className="border-2 border-blue-700 col-span-4 row-span-5 p-8">
-        {element}
-      </main>
+      <Main element={element} />
     </div>
   );
 };
