e1a78dcb47db530912c3d78174d11540d0376d62
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-15 10:04:43 +0330

    refactor: Update HomePage component with improved content

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,25 @@
 const HomePage = () => {
-  return <div>Home page</div>;
+  return (
+    <div className="text-lg tracking-wide flex flex-col gap-2 w-1/2">
+      <p>
+        Born in 1999 in Urmia, Iran, I am Amin, a software engineer who believes
+        in the power of well-designed architecture to tackle any problem through
+        software. 
+      </p>
+      <p>
+        Before writing a single line of code, I always question the
+        problem it will solve. 
+      </p>
+      <p>
+        I am driven to use my skills to create impactful
+        solutions.
+      </p>
+      <p>
+        My best teacher is my experiences I believe without mistakes
+        there is no learning.
+      </p>
+    </div>
+  );
 };
 
 export default HomePage;
