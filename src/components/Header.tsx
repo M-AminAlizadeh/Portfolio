160689cece2d4127f99e5ce7aa9601109cbac6c5
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-15 09:05:45 +0330

    refactor: Extract Header component for reusability

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="font-mono border-2 col-span-5 row-span-1">
+        <h1 className="text-6xl">Amin Alizadeh</h1>
+        <h4 className="italic capitalize">software engineer</h4>
+    </header> 
+  )
+}
+
+export default Header
+
