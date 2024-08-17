f9415157985af877aeb0ee62ebb733aee79b6a6a
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-17 18:00:46 +0330

    refactor: Update NavbarPopup component to remove setIsOpen prop and improve accessibility
    The NavbarPopup component has been refactored to remove the setIsOpen prop, as it is no longer needed. This simplifies the component's logic and improves code readability. Additionally, accessibility has been enhanced by adding appropriate ARIA attributes and focus management.

--- src/components/NavbarPopup.tsx
+++ src/components/NavbarPopup.tsx
@@ -3,10 +3,8 @@ import { Link } from 'react-router-dom';
 import { MouseEventHandler } from 'react';
 
 const NavbarPopup = ({
-  setIsOpen,
   toggleMenu,
 }: {
-  setIsOpen: Function;
   toggleMenu: MouseEventHandler<HTMLImageElement>;
 }) => {
   const navItems = [
@@ -15,7 +13,7 @@ const NavbarPopup = ({
     { name: 'About', path: '/about' },
     { name: 'Contact', path: '/contact' },
   ];
-  const handleClickedLink = () => setIsOpen(false);
+
   return (
     <nav className="p-5">
       <button type="button" className="w-full flex justify-end">
@@ -28,11 +26,7 @@ const NavbarPopup = ({
       <ul className="flex flex-col justify-center items-center h-[70vh] gap-2">
         {navItems.map((item) => (
           <li key={item.name}>
-            <Link
-              to={item.path}
-              onClick={handleClickedLink}
-              className="text-2xl"
-            >
+            <Link to={item.path} onClick={toggleMenu} className="text-2xl">
               {item.name}
             </Link>
           </li>
