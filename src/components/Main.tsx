c27f878fb3d766d980c1769ecc3ef7034a9e3f75
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-17 18:27:35 +0330

    refactor: Remove unnecessary border-2 class from Main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,6 @@
 const Main = ({ element }: { element: React.ReactNode }) => {
   return (
-    <main className="border-2 col-span-5 sm:col-span-4 row-span-5 p-8">
+    <main className="col-span-5 sm:col-span-4 row-span-5 p-8">
       {element}
     </main>
   );
