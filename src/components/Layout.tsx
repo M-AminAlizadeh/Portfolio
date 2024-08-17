567e885a7331f508577b76ffaddf2a9b5b022851
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-17 14:02:03 +0330

    feat: Add dark mode toggle functionality to Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,29 @@
+import { useState, useEffect } from 'react';
 import Header from './Header';
 import Main from './Main';
 import Navbar from './Navbar';
 
 const Layout = ({ element }: { element: React.ReactNode }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const handleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [isDarkMode]);
+
   return (
-    <div className="border-2 min-h-[90vh] m-10 p-10 grid grid-cols-5 grid-rows-6">
+    <div className="border dark:border-white border-black min-h-[90vh] m-10 p-10 grid grid-cols-5 grid-rows-6">
       <Header />
       <Navbar />
       <Main element={element} />
+      <button onClick={handleDarkMode} className="border-2">
+        toggler dark/ligh mode
+      </button>
     </div>
   );
 };
