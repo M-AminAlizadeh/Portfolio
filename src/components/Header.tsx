d570d52fdf00e9aaafed9f4cf044b145023eeec1
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-17 15:23:02 +0330

    refactor: Update Header component to improve font sizes and add select-none class and make this component fully responsive on various devices

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
 const Header = () => {
   return (
-    <header className="font-mono col-span-5 row-span-1">
-      <h1 className="text-6xl">Amin Alizadeh</h1>
-      <h4 className="italic capitalize">software engineer</h4>
+    <header className="font-mono col-span-5 row-span-1 select-none">
+      <h1 className="text-lg sm:text-4xl lg:text-5xl 2xl:text-7xl">
+        Amin Alizadeh
+      </h1>
+      <h4 className="text-sm sm:text-lg lg:text-xl 2xl:text-2xl italic capitalize">
+        software engineer
+      </h4>
     </header>
   );
 };
