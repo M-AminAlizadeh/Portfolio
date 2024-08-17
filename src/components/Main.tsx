5e1859060ef78ff759574ef94f0f28ada01a04a7
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-17 17:59:31 +0330

    refactor: Update Main component to adjust column span and improve responsiveness

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,9 @@
 const Main = ({ element }: { element: React.ReactNode }) => {
-  return <main className="border-2 col-span-4 row-span-5 p-8">{element}</main>;
+  return (
+    <main className="border-2 col-span-5 sm:col-span-4 row-span-5 p-8">
+      {element}
+    </main>
+  );
 };
 
 export default Main;
