faad6c2e920c5544f6e7a96c151be507b863dbe5
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-19 12:06:41 +0330

    Fix the formatter and linters errors

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,9 +3,11 @@ import '../404.css';
 
 const _404 = () => {
   return (
-    <div className='_404-container'>
+    <div className="_404-container">
       <div title="404">404</div>
-      <Link to='/' className='text-5xl hover:underline'>Back to home page</Link>
+      <Link to="/" className="text-5xl hover:underline">
+        Back to home page
+      </Link>
     </div>
   );
 };
