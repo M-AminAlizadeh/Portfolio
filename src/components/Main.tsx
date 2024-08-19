2d8e0ee1f9f19218d8ac1b886477a91cb9415607
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-19 11:17:57 +0330

    refactor: Simplify Main component layout for better responsiveness

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,6 @@
 const Main = ({ element }: { element: React.ReactNode }) => {
   return (
-    <main className="border border-black col-span-5 sm:col-span-4 row-span-5 p-8">
-      {element}
-    </main>
+    <main className="col-span-5 sm:col-span-4 row-span-5 p-8">{element}</main>
   );
 };
 
