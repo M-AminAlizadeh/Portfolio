7adfbffc7fcea9d822a5132383869e5357feac3b
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-19 12:06:02 +0330

    refactor: Improve 404 page layout and add link to home page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,13 @@
+import { Link } from 'react-router-dom';
+import '../404.css';
+
 const _404 = () => {
-  return <div>404</div>;
+  return (
+    <div className='_404-container'>
+      <div title="404">404</div>
+      <Link to='/' className='text-5xl hover:underline'>Back to home page</Link>
+    </div>
+  );
 };
 
 export default _404;
