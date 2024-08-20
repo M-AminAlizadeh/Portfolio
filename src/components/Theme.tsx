03fd32ad72f5aecec624a129d6dbcf1b36d0d353
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-20 09:03:27 +0330

    refactor: Update Theme component to use radio buttons for dark/light mode selection

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -1,43 +1,47 @@
-import { useState } from "react"
+import { useState } from 'react';
 
-const Theme = ({setIsDarkMode}: { setIsDarkMode: React.Dispatch<React.SetStateAction<boolean>> }) => {
-  const [theme, setTheme] = useState('light')
+const Theme = ({
+  setIsDarkMode,
+}: {
+  setIsDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const [theme, setTheme] = useState('light');
   const handleTheme = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTheme(e.target.value)
-    if(e.target.value === 'dark') {
-      setIsDarkMode(true)
+    setTheme(e.target.value);
+    if (e.target.value === 'dark') {
+      setIsDarkMode(true);
     } else {
-      setIsDarkMode(false)
+      setIsDarkMode(false);
     }
-  }
+  };
   return (
     <div className="border-2 border-white absolute bottom-12 -left-20 flex flex-row-reverse gap-3  -rotate-90">
       <div className="flex flex-row-reverse gap-1">
         <label htmlFor="dark">Dark</label>
-        <input 
+        <input
           id="dark"
           name="theme"
           type="radio"
-          value='dark' 
+          value="dark"
           checked={theme === 'dark'}
           onChange={handleTheme}
           className="cursor-pointer"
         />
-      </div>      
+      </div>
       <div className="flex flex-row-reverse gap-1">
         <label htmlFor="light">Light</label>
         <input
           id="light"
           name="theme"
-          type="radio" 
+          type="radio"
           value="light"
           checked={theme == 'light'}
           onChange={handleTheme}
           className="cursor-pointer"
         />
-      </div>      
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Theme
+export default Theme;
