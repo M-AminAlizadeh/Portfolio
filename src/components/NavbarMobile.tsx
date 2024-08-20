1d8ead4705f6d2ca7ad491954be939e82a21ab37
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-20 13:04:17 +0330

    refactor: Update NavbarMobile component to use global state for menu toggle

--- src/components/NavbarMobile.tsx
+++ src/components/NavbarMobile.tsx
@@ -1,7 +1,13 @@
-const NavbarMobile = ({ toggleMenu }: { toggleMenu: () => void }) => {
+import { useGlobalState } from '../context/GlobalStatesContext';
+
+const NavbarMobile = () => {
+  const {
+    isOpen,
+    setIsOpen,
+  } = useGlobalState();
   return (
     <div className="col-span-1 row-span-1 flex justify-center items-center sm:hidden">
-      <button onClick={toggleMenu}>
+      <button onClick={()=>setIsOpen(!isOpen)}>
         <img
           width="50"
           src="https://img.icons8.com/ios/50/menu--v7.png"
