7a04ea78e601c938251ea6ca3f0bf45bfd0b784c
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-20 13:10:20 +0330

    refactor: Update NavbarMobile component to use global state for menu toggle and dynamically set the menu icon color based on dark mode state

--- src/components/NavbarMobile.tsx
+++ src/components/NavbarMobile.tsx
@@ -1,13 +1,13 @@
 import { useGlobalState } from '../context/GlobalStatesContext';
 
 const NavbarMobile = () => {
-  const { isOpen, setIsOpen } = useGlobalState();
+  const { isOpen, setIsOpen, isDarkMode } = useGlobalState();
   return (
     <div className="col-span-1 row-span-1 flex justify-center items-center sm:hidden">
       <button onClick={() => setIsOpen(!isOpen)}>
         <img
           width="50"
-          src="https://img.icons8.com/ios/50/menu--v7.png"
+          src={`https://img.icons8.com/ios/50/${isDarkMode ? 'ffffff' : '000000'}/menu--v7.png`}
           alt="menu--v7"
         />
       </button>
