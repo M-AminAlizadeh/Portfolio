5349ef3109a4f3d80b5aa4b86b128ee4d082fc86
m-aminalizadeh <aminalizadeh@riseup.net>
2024-08-20 10:41:24 +0330

    refactor: Update ContactForm component to include select-none class for better user experience

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -5,7 +5,7 @@ const ContactForm = () => {
     <form
       action={formspreeUrl}
       method="POST"
-      className="flex-1 flex flex-col p-4 w-full max-w-md"
+      className="select-none flex-1 flex flex-col p-4 w-full max-w-md"
     >
       <div className="mb-4">
         <label
